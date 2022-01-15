cbf99892d912bd4c4da484428c3f3b4f52c4c6f8
Abril♥ <abrilmartinagil@gmail.com>
2022-01-15 13:00:35 -0300

    Create product.routes.js

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,6 +1,11 @@
 const express = require('express');
-const productController = require('../controller/product.controller');
+const { postProduct, getProductId, putProduct, deleteProduct} = require('../controller/product.controller');
 
 const router = express.Router();
 
-router.get('/product', productController)
+router.get('/product', getProductId)
+router.post('/product', postProduct)
+router.put('/product', putProduct)
+router.delete('/product', deleteProduct)
+
+
