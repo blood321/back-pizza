2290fcc29724d982c4946edbfc8d1be6d5dea7db
Abril♥ <abrilmartinagil@gmail.com>
2022-01-15 14:02:39 -0300

    Update order.routes.js

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const orderController = require('../controller/order.controller.js');
+const {postOrder, getOrderId, putOrder, deleteOrder} = require('../controller/order.controller');
 
 const router = express.Router();
 
-router.get('/order', orderController)
+router.get('/order', getOrderId);
+router.post('/order', postOrder);
+router.put('/order', putOrder);
+router.delete('/order', deleteOrder);
